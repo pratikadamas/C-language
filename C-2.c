43bfba1593be992bae7e7c321e1870499f54f80d
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2023-10-11 11:29:37 +0530

    Create C-2.c

---
+++ C-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a,b,c,d,e,f;
+  printf("enter valu of a,b");
+  scanf("%d %d",&a,&b);
+
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("value of c=%d d=%d e=%d f=%d",c,d,e,f);
+}
