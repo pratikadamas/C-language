060b23412d89c9ee9a19a607789822e6908c2b60
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2023-11-04 13:21:45 +0530

    Create C-15.c

---
+++ C-15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float ruppees;
+printf("Enter the amount in Ruppees:\n");
+scanf("%f",&ruppees);
+printf("Amount in Dollars:%f\n",ruppees*0.012);
+printf("Amount in Euros:%f\n",ruppees*0.011);
+printf("Amount in Yans:%f\n",ruppees*1.77);
+printf("Amount in Rubels:%f\n",ruppees*1.16);
+}
