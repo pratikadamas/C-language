46114094c5de28e753663da3ed19a5fb81f000ad
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2023-11-21 14:02:32 +0530

    Create C-24.c

---
+++ C-24.c
@@ -0,0 +1,20 @@
+// sum of n natural number
+#include <stdio.h>
+int sum(int a){
+    int x;
+    if(a==1){
+        return 1;
+    }
+    return x=a+sum(a-1);
+}
+
+int main() {
+int n,c;
+printf("ENTER RANGE 0F N NUMBER=");
+scanf("%d",&n);
+c=sum(n);
+printf("%d",c);
+
+
+
+}
