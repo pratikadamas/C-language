a28b2e640ceebf49f8e2b5775ea68b26eb04dd08
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2023-11-25 00:24:05 +0530

    Update C-09.c

--- C-09.c
+++ C-09.c
@@ -1,1 +1,13 @@
+/**85
+wap in c to implement macros and solve an arithmetical operation through it.
+ **/
+#include<stdio.h>
+#define d(x) (x*x*x)
+#define e(x,y) (x+y-x*x-y+x*x)
 
+int main()
+{
+    int a = 5;
+    printf("\n%d",e(a,a+5));
+    printf("\n%d",d(a-1));
+}
