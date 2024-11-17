1fba09b4e7c200176c686a7ac386d3b7a55ccf0a
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2024-11-17 13:12:31 +0530

    Bisection Method

--- C-35 bisection method.c
+++ C-35 bisection method.c
@@ -1,6 +1,4 @@
-//
-// Created by Pratik Giri on 08-11-2024.
-//
+
 #include "stdio.h"
 #include "math.h"
 float f(float x){
@@ -13,25 +11,33 @@ int main(){
     scanf("%f",&a);
     printf(" value of b = ");
     scanf("%f",&b);
+
+int n=0;
+    printf("\n      n         an         bn    Xn+1=(an+bn)/2    f(Xn+1)     \n");
     if(f(a)*f(b)<0){
+
         while(fabs(b-a)>error)
         {
+
+            printf("      %d        %.3f      %.3f    ",n,a,b);
                 c = (a + b) / 2;
+
+            printf("    %.3f        %f \n",c,f(c));
                 if (f(c) == 0)
                 printf(" ROOT is c= %f", c);
 
-                else if (f(a) * f(c) < 0)
+                else if (f(a) * f(c) < 0) {
                     b = c;
-            else
-                a = c;
-
-//            if (fabs(b - a) < error)
-          // printf(" root is %f", c);
-
+                }
+            else {
+                    a = c;
+                }
+n++;
         }
 
         printf(" root is %f ", c);
     }
+
     else{
         printf(" Invalid interval selection\n");
     }
