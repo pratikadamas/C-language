453c9db723f811438ea1fcb0ace3c8346c693989
Pratik Giri <pratikgiri585@gmail.com>
2026-01-22 23:50:28 +0530

    selection sort

--- C-53 selection sort.c
+++ C-53 selection sort.c
@@ -2,25 +2,30 @@
 // Created by Pratik Giri on 04-02-2025.
 //
 #include "stdio.h"
-int main(){
-    int n=8;
-    int arr[] ={ 56,36,-9,56,123,-78,99,-102};
-    for (int i=0;i<n-1 ;++i)
-    {
-        for (int j=i+1; j<n; ++j)
-        {
-            if(arr[j]<arr[i])
-            {
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+int main() {
+    int n = 8,k=0;
+
+    int arr[] = {100,99,98,95,94,91,90,89};
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[i]) {
+                k++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
             }
         }
+        printf("\nstep i=%d\n", i + 1);
+        for (int i = 0; i < n; ++i) {
+            printf(" %d ", arr[i]);
+
+        }
     }
-    for (int i = 0; i < n; ++i)
-    {
-        printf(" %d ",arr[i]);
+    printf("\nfinal array \n");
+    for (int i = 0; i < n; ++i) {
+        printf(" %d ", arr[i]);
 
     }
-
+    printf("\n k= %d",k);
 }
