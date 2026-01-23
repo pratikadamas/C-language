fcc501f19df5872168c4f642e3c9fa2902a96754
Pratik Giri <pratikgiri585@gmail.com>
2026-01-23 19:50:01 +0530

    root finding

--- C-48 Nuton method for root finding.c
+++ C-48 Nuton method for root finding.c
@@ -1,15 +1,19 @@
-//
-// Created by Pratik Giri on 15-11-2024.
-//
+
 #include "stdio.h"
 #include "math.h"
-float f(float x){
-    return x*x*x-2*x-5;
-}
-float g(float x){
-    return 3*x*x-2;
-}
-int main (){
+float f(float x)
+  {
+    return x*x*x*x-x-10;
+
+  }
+
+  float g(float x)
+  {
+    return 4*x*x*x-1;
+ }
+int main ()
+
+{
 
     float x,Xo,Y,Err_tol;
     printf(" Enter the initial value of Xo = ");
@@ -19,18 +23,18 @@ int main (){
     scanf("%f",&Err_tol);
 
 
-    printf("  n         Xn             f(Xn)              Xn+1        \n");
+    printf("  n    |     Xn       |      f(Xn)      |        Xn+1        \n");
+
     int n=0;
     Y= f(Xo);
-    while(fabs(Y)>Err_tol){
-n++;
-
-
+    while(fabs(Y)>Err_tol)
+    {
+        n++;
         x=Xo-f(Xo)/g(Xo);
       //  printf("%f  %d \n",x,i++);
         Y= f(x);
         Xo=x;
-        printf("   %d       %f          %f          %f\n",n,Xo,Y,x);
+        printf("   %d        %f           %f          %f\n",n,Xo,Y,x);
     }
 
     printf("\nThe value of root is %f ",Xo);
